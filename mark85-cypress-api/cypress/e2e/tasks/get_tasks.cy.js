b0bcf1986fe230598c163bb686c1f18ff1319b84
João Tadeu Soares Pereira <ojoaotadeu@gmail.com>
2024-06-18 19:28:39 -0300

    refatorando codigo

--- mark85-cypress-api/cypress/e2e/tasks/get_tasks.cy.js
+++ mark85-cypress-api/cypress/e2e/tasks/get_tasks.cy.js
@@ -10,7 +10,7 @@ describe('GET /tasks', () => {
 
     context('Quando quero obter minhas tasks', function () {
 
-        it('GET tasks', function () {
+        it('GET /tasks', function () {
             const { user, tasks } = this.tasks.list
 
             cy.task('deleteTaskLike', 'Pagar')
@@ -20,24 +20,84 @@ describe('GET /tasks', () => {
             cy.postSession(user)
                 .then(responseUser => {
 
-                    tasks.forEach(function(t) {
+                    tasks.forEach(function (t) {
                         cy.postTask(t, responseUser.body.token)
                     })
 
-                    cy.api({
-                        url: '/tasks',
-                        method: 'GET',
-                        headers: {
-                            authorization: responseUser.body.token
-                        },
-                        failOnStatusCode: false
-                    }).then( response => {
+                    cy.getTasks(responseUser.body.token).then(response => {
                         expect(response.status).to.eq(200)
                     }).its('body')
-                        .should('be.an','array')
+                        .should('be.an', 'array')
                         .and('have.length', tasks.length)
                 })
         })
+
+    })
+
+    context('Quando quero obter minhas tasks pelo ID', function () {
         
+        it('GET /tasks/:id Unique Task', function () {
+            const { user, task } = this.tasks.unique
+
+            cy.task('deleteTask', task.email, user.email)
+            cy.task('deleteUser', user.email)
+            cy.postUser(user)
+            cy.postSession(user)
+                .then(userResponse => {
+
+                    cy.postTask(task, userResponse.body.token)
+                        .then(taskResponse => {
+                            cy.getUniqueTask(taskResponse.body._id, userResponse.body.token)
+                                .then(response => {
+                                    expect(response.status).to.eq(200)
+                                })
+                        })
+                })
+        })
+
+    })
+
+    context('Quando quero obeter minhas tarefas pelo GET', function () {
+
+        it('GET /tasks/ not_found', function () {
+
+            const { user, task } = this.tasks.not_found
+
+            cy.task('deleteTask', task.email, user.email)
+            cy.task('deleteUser', user.email)
+            cy.postUser(user)
+
+            cy.postSession(user)
+                .then(userResponse => {
+
+                    cy.postTask(task, userResponse.body.token)
+                        .then(taskResponse => {
+
+                            cy.api({
+                                url: '/tasks/' + taskResponse.body._id,
+                                method: 'DELETE',
+                                headers: {
+                                    authorization: userResponse.body.token
+                                },
+                                failOnStatusCode: false
+                            }).then(response => {
+                                expect(response.status).to.eq(204)
+                            })
+
+                            cy.api({
+                                url: '/tasks/' + taskResponse.body._id,
+                                method: 'GET',
+                                headers: {
+                                    authorization: userResponse.body.token
+                                },
+                                failOnStatusCode: false
+                            }).then(response => {
+                                expect(response.status).to.eq(404)
+                            })
+
+                        })
+                })
+        })
+
     })
 })
--- mark85-cypress-api/cypress/support/commands.js
+++ mark85-cypress-api/cypress/support/commands.js
@@ -28,3 +28,37 @@ Cypress.Commands.add('postTask', (task, token) => {
     failOnStatusCode: false
   }).then(response => { return response })
 })
+
+Cypress.Commands.add('deleteTask', (task, token) => {
+  cy.api({
+    url: '/tasks',
+    method: 'DELETE',
+    body: task,
+    headers: {
+      authorization: token
+    },
+    failOnStatusCode: false
+  }).then(response => { return response })
+})
+
+Cypress.Commands.add('getUniqueTask', (taskId, token) => {
+  cy.api({
+    url: '/tasks/' + taskId,
+    method: 'GET',
+    headers: {
+      authorization: token
+    },
+    failOnStatusCode: false
+  }).then(response => { return response })
+})
+
+Cypress.Commands.add('getTasks', (token) => {
+  cy.api({
+    url: '/tasks',
+    method: 'GET',
+    headers: {
+      authorization: token
+    },
+    failOnStatusCode: false
+  })
+})
